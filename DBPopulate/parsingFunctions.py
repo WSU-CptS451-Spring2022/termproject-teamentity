7ccb650c7d4362f23c8f441ccae3f904ca5505f7
brianlopezwsu <55165785+brianlopezwsu@users.noreply.github.com>
2022-03-29 20:45:04 -0700

    Add files via upload

--- DBPopulate/parsingFunctions.py
+++ DBPopulate/parsingFunctions.py
@@ -1,4 +1,5 @@
 import json
+from os import system
 import psycopg2
 from flatten_json import flatten
 
@@ -13,7 +14,7 @@ def int2BoolStr (value):
     
 def usersTableParse():
     print("ALERT: Now Beginning Users Parse Function")
-    with open('./usersTable.JSON', 'r') as f:
+    with open('yelp_user.JSON', 'r') as f:
         line = f.readline()
         count_line = 0
         
@@ -27,10 +28,10 @@ def usersTableParse():
             data = json.loads(line)
             time = data['yelping_since'].split(' ')
             
-            users = "INSERT INTO users (avg_stars, cool, funny, totalLikes, fans, user_latitude, user_longitude, userName, tipCount, useful, user_id, yelping_since" \
-                "startInYelp, latitude, longitude)" "VALUES (" + str(data["average_stars"]) + "," + str(data["cool"]) + "," str(data["funny"]) + ", 0," + \ 
-                str(data["fans"]) + ", '0', '0','" + cleanStr4SQL(data["name"]) + "'," + str(data["tipCount"]) + "," + str(data["useful"]) + ",'" + cleanStr4SQL(data["user_id"]) + \
-                    "','" + str(time[0]) + "','" + str(time[1]) + "'0, 0");"
+            users = "INSERT INTO Users(averageStars, cool, funny, totalLikes, fans, user_latitude, user_longitude, username, totalTips, useful, user_id, accountDate" \
+                "startInYelp, latitude, longitude)" "VALUES (" + str(data["average_stars"]) + "," + str(data["cool"]) + "," + str(data["funny"]) + ", 0," \
+                + str(data["fans"]) + ", '0', '0','" + cleanStr4SQL(data["name"]) + "'," + str(data["tipcount"]) + "," + str(data["useful"]) + ",'" + cleanStr4SQL(data["user_id"]) + \
+                    "','" + str(time[0]) + "','" + str(time[1]) + "'0, 0);"
                     
             try:
                 current.execute(users)
@@ -44,12 +45,12 @@ def usersTableParse():
             
         current.close()
         conn.close()
-    print(count_lines)
-    f.close()
+        print(count_line)
+        f.close()
     
 def tipsTableParse():
     print("ALERT: Now Beginnning Tips Table Parse Function")
-    with open('./tips.JSON', 'r') as f:
+    with open('./yelp_tip.JSON', 'r') as f:
     
         line = f.readline()
         count_line = 0
@@ -64,10 +65,10 @@ def tipsTableParse():
             data = json.loads(line)
             time = data['date'].split(' ')
             
-            tips = "INSERT INTO (business_id, user_id, likes, tipText, tipDate, tipTime)" \
+            tips = "INSERT INTO Tips(business_id, user_id, likesCount, textOfTips, dateOfTips, timeOfTips)" \
                 "VALUES ('" + cleanStr4SQL(data["business_id"]) + "','" + cleanStr4SQL(data["user_id"]) + "'," + str(data["likes"]) + ",'" + \
-                    cleanStr4SQL(data["text"].encode('unicode_escape').decode('unicode_escape')) + "','" + cleanStr4SQL(time[0]) + "','" \
-                    cleanStr4SQL(time]) +"');"
+                    cleanStr4SQL(data["text"].encode('unicode_escape').decode('unicode_escape')) + "','" + cleanStr4SQL(time[0]) + "','" + \
+                    cleanStr4SQL(time[1]) + "');"
             
             try:
                 current.execute(tips)
@@ -86,7 +87,7 @@ def tipsTableParse():
 
 def checkInTableParse():
     print("ALERT: Now Beginning Check In Table Parse Function")
-    with open('./checkIn.JSON', 'r') as f:
+    with open('yelp_checkin.JSON', 'r') as f:
         line = f.readline()
         count_line = 0
         
@@ -101,10 +102,10 @@ def checkInTableParse():
             time = data['date'].split(' ')
             
             for t in time:
-                time = x.split(' ')
+                time = t.split(' ')
                 date = time[0].split('-')
                 
-                checkIn = "INSERT INTO checkIn (business_id, c_year, c_date, c_month, c_time) " "VALUES ('" + cleanStr4SQL(data["business_id"]) +\
+                checkIn = "INSERT INTO CheckIn(business_id, yearOfCheckIn, dateOfCheckIn, monthOfCheckIn, timeOfCheckIn) " "VALUES ('" + cleanStr4SQL(data["business_id"]) +\
                     "','" + str(date[0]) + "'," + str(date[2]) + ",'" + cleanStr4SQL(date[1]) + "','" + cleanStr4SQL(time[1]) + "');"
                 
                 try:
@@ -125,7 +126,7 @@ def checkInTableParse():
     
 def friendTableParse():
     print("ALERT: Now Beginning Friend Table Parse Function")
-    with open('./users.JSON', 'r') as f:
+    with open('yelp_user.JSON', 'r') as f:
         line = f.readline()
         count_line = 0
         
@@ -133,12 +134,13 @@ def friendTableParse():
             conn = psycopg2.connect("dbname='business' user='postgres' host='localhost' password='project'")
         except:
             print("ERROR: Connection to Database Unsuccessful")
+        current = conn.cursor()
         
         while line:
             data = json.loads(line)
             
             for friend in data['friends']:
-                friend = "INSERT INTO friends (user_id, friend_id)" "VALUES ('" + cleanStr4SQL(data["user_id"]) + "','" + cleanStr4SQL(friend_id) + "');"
+                friend = "INSERT INTO Friend(user_id, friend_id)" "VALUES ('" + cleanStr4SQL(data["user_id"]) + "','" + cleanStr4SQL(friend) + "');"
                 
                 try:
                     current.execute(friend)
@@ -237,31 +239,32 @@ def parseTipData():
         
 def insertBusinessTable():
     print("ALERT: Beginning INSERT to Business Table Function ")
-    with open("./business.JSON", "r") as f:
+    with open('yelp_business.JSON', 'r') as f:
         line = f.readline()
         count_line = 0
         
         try:
-            conn.psycopg2.connect("dbname='business' user='postgres' host='localhost' password='project'")
+            conn = psycopg2.connect("dbname='business' user='postgres' host='localhost' password='project'")
         except:
-            print("ERROR: INSERT to Business Table Unsuccessful")
+            print("ERROR: Connection to Database Unsuccessful")
         current = conn.cursor()
         
         while line: 
             data = json.loads(line)
-            business = "INSERT INTO business (business_id, name, address, state, city, zipcode, latitude, longtitude, stars, numCheckins, numTips, is_open" \
-                "VALUES ('" + cleanStr4SQL(data['business_id']) + "','" + cleanStr4SQL(data["name"]) + "','" + cleanStr4SQL(data["address"]) + "','" \
-                    cleanStr4SQL(data["state"]) + "','" + cleanStr4SQL(data["city"]) + "','" + data["postal_code"] + "'," + str(data["latitude"]) + "," \
-                        str(data["longitude"]) +"," + str(data["stars"])) +", 0 , 0 ," + str(data["is_open"]) + ", 0 );"
+            business = "INSERT INTO Business(business_id, business_name, addressOfBusiness, stateOfBusiness, city, zipCode, latitude, longitude, stars, numCheckins, numberTips, ifopen, reviewCount)  \
+                VALUES ('" + cleanStr4SQL(data['business_id']) + "','" + cleanStr4SQL(data["name"]) + "','" + cleanStr4SQL(data["address"]) + "','" + \
+                    cleanStr4SQL(data["state"]) + "','" + cleanStr4SQL(data["city"]) + "','" + data["postal_code"] + "'," + str(data["latitude"]) + "," + \
+                        str(data["longitude"]) + "," + str(data["stars"]) +", 0 , 0 ," + str(data["is_open"]) + ", 0 );"
             try:
                 current.execute(business)
             except:
-                printf("ERROR: INSERT to Business Table Unsuccessful")
+                print("ERROR: INSERT to Business Table Unsuccessful")
+                system.exit()
             conn.commit()
             
-            categories = data["categories"].split(' ')
+            categories = data["categories"].split(', ')
             for c in categories:
-                business_category = "INSERT INTO businessCategory(business_id, category) VALUES ('" + data['business_id'] +"', '" + cleanStr4SQL(str(c)) + "'"); "
+                business_category = "INSERT INTO Categories(business_id, category_name) VALUES ('" + data['business_id'] + "', '" + cleanStr4SQL(str(c)) + "'""); "
                 
                 try:
                     current.execute(business_category)
@@ -274,15 +277,15 @@ def insertBusinessTable():
             for k, v in flat.items():
                 if(k and v):
                     if(k.startswith("GoodForMeal")):
-                        if(key == 'GoodForMeal'):
+                        if(k == 'GoodForMeal'):
                             updatedKey = (k, k)
                         else:
                             updatedKey = k.split('_')
                         if (v == 'None'):
                             updatedValue = 'False'
                         else:
-                            updatedValue = value
-                            meal = "INSERT INTO Categories(business_id, category_name) VALUES ('" + data['business_id'] + "','" + str(updatedKey[1]) + \
+                            updatedValue = v
+                            meal = "INSERT INTO GoodMeals(business_id, typeOfMeal, valueOfGoodMeals) VALUES ('" + data['business_id'] + "','" + str(updatedKey[1]) + \
                                 "'," + cleanStr4SQL(str(updatedValue)) + "); "
                         try:
                             current.execute(meal)
@@ -295,11 +298,11 @@ def insertBusinessTable():
                             updatedKey = (k, k)
                         else:
                             updatedKey = k.split('_')
-                        if(v = 'None'):
+                        if(v == 'None'):
                             updatedValue = 'False'
                         else:
                             updatedValue = v
-                            ambience = "INSERT INTO ambience(business_id, a_type, a_value) VALUES ('" + data['business_id'] + "','" + str(updatedKey[1]) + "'," + \
+                            ambience = "INSERT INTO Ambience(business_id, typeOfAmbience, valueOfAmbience) VALUES ('" + data['business_id'] + "','" + str(updatedKey[1]) + "'," + \
                                 cleanStr4SQL(str(updatedValue)) + ");"
                         try:
                             current.execute(ambience)
@@ -316,7 +319,7 @@ def insertBusinessTable():
                         else:
                             updatedValue = v
                         
-                        parking = "INSERT INTO parking(business_id, p_type, p_val) VALUES ('" + data['business_id'] + "','" + str(updatedKey[1]) + "'," + \
+                        parking = "INSERT INTO parking(business_id, typeOfParking, valueOfParking) VALUES ('" + data['business_id'] + "','" + str(updatedKey[1]) + "'," + \
                             cleanStr4SQL(str(updatedValue)) + ");"
                         try:
                             current.execute(parking)
@@ -325,8 +328,8 @@ def insertBusinessTable():
                             print(parking)
                         
                     else:
-                        attribute = "INSERT INTO attributes(business_id, a_name, a_value) VALUES ('" + data['business_id'] + "','" + str(k) + "','" + \
-                            cleanStr4SQL(str(updatedValue)) + "'""); "
+                        attribute = "INSERT INTO Attributes(business_id, nameOfAttribute, valueOfAttribute) VALUES ('" + data['business_id'] + "','" + str(k) + "', '" \
+                            + cleanStr4SQL(str(v)) + "'""); "
                         try:
                             current.execute(attribute)
                         except:
@@ -338,8 +341,8 @@ def insertBusinessTable():
             for k, v in data['hours'].items():
                 if(k and v):
                     time = v.split('-')
-                    hours = "INSERT INTO hours(business_id, day, openTime, closeTime" VALUES ('" + data['business_id'] + "','" + str(k) + "','" + \
-                        cleanStr4SQL(str(time[0])) + "','" + cleanStr4SQL(str(time[0])) + "'""); "
+                    hours = "INSERT INTO HoursOfBusiness(business_id, dayOfBusiness, openTime, closeTime) VALUES ('" + data['business_id'] + "','" + str(k) + "','" \
+                        + cleanStr4SQL(str(time[0])) + "','" + cleanStr4SQL(str(time[0])) + "'""); "
                     try:
                         current.execute(hours)
                     except:
@@ -354,7 +357,8 @@ def insertBusinessTable():
         f.close()
             
         
-                        
+if __name__ == "__main__":
+    insertBusinessTable()                       
                         
                                                                                         
                                                                                                  
